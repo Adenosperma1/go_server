47602fc37119db985b14e22aadd5870fee77c28d
Brendan Wilde <44901073+Adenosperma1@users.noreply.github.com>
2024-05-21 23:24:34 +1000

    ...

--- server.go
+++ server.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
+func handler(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Hello %s, welcome to the server.", r.URL.Path[1:])
+}
+
 func main(){
-	fmt.Println("hi")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":9000", nil)
+
+	//run this in terminal to start the server
+	//go to localhost:9000/Brendan in a browser
 }
